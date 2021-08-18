b27408237a358c84b9e63743fcbdfc08d0a06fa7
Masatoshi Katabuchi <mattocci27@gmail.com>
2021-08-18 17:15:18 +0000

    fixed docker

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -7,14 +7,14 @@ AUTHOR="Katabuchi, M."
 LASTNAME=Katabuchi
 
 # ref_citeproc_edit.md
-pandoc sources/ref.md -F pandoc-crossref --citeproc \
+pandoc sources/ref.md --citeproc \
 	--bibliography=sources/ref.bib \
 	--csl=templates/my_ref.csl \
 	--pdf-engine=xelatex \
-	-s -o outputs/ref.tex
+	-s -o ./outputs/ref.tex
 
 pandoc outputs/ref.tex -o outputs/ref_citeproc.md
-rm -f outputs.ref_citeproc_edit.md
+rm -f outputs/ref_citeproc_edit.md
 python3 scripts/ref_edit.py $AUTHOR
 
 # CV.tex
@@ -23,17 +23,17 @@ pandoc -s sources/CV1.md outputs/ref_citeproc_edit.md sources/CV2.md --citeproc 
 	--from markdown --to context \
 	--pdf-engine=xelatex \
 	-V papersize=letter \
-	-o outputs/${LASTNAME}_CV.tex
+	-o ./outputs/${LASTNAME}_CV.tex
 
 # CV.html
 pandoc --standalone sources/CV1.md outputs/ref_citeproc_edit.md sources/CV2.md \
   -H templates/CV_template.css \
   --from markdown --to html \
-  -o outputs/${LASTNAME}_CV.html
+  -o ./outputs/${LASTNAME}_CV.html
 
 # CV.docx
 pandoc -s sources/CV1.md outputs/ref_citeproc_edit.md sources/CV2.md \
-  --reference-doc=templates/reference.docx -o outputs/${LASTNAME}_CV.docx
+  --reference-doc=templates/reference.docx -o ./outputs/${LASTNAME}_CV.docx
 
 # CV.rtf
-pandoc -s sources/CV1.md outputs/ref_citeproc_edit.md sources/CV2.md -o outputs/${LASTNAME}_CV.rtf
+pandoc -s sources/CV1.md outputs/ref_citeproc_edit.md sources/CV2.md -o ./outputs/${LASTNAME}_CV.rtf
